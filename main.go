787314d0a80e1c22ccca8e151f039d612d717db5
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-11-20 17:55:11 +0900

    gin setup

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
-import "fmt"
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	fmt.Println("yahho")
+	r := gin.Default()
+	// web
+	r.Use(cors)
+	r.GET("/alive", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ヤッホー!!",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func cors(c *gin.Context) {
+	headers := c.Request.Header.Get("Access-Control-Request-Headers")
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,PATCH,DELETE,OPTIONS")
+	c.Header("Access-Control-Allow-Headers", headers)
+	if c.Request.Method == "OPTIONS" {
+		c.Status(200)
+		c.Abort()
+	}
+	c.Set("start_time", time.Now())
+	c.Next()
+
 }
