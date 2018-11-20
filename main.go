346d9c22db66f1a8b12dbca6066361b28ee58895
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-11-20 20:50:17 +0900

    upload までできた。

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/HAL-Future-Creation-Exhibition/s3-upload-server/util"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -21,6 +24,37 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "ヤッホー!!",
 		})
+	})
+	r.POST("/upload", func(c *gin.Context) {
+		myS3, err := util.NewS3(os.Getenv("S3ACCESSKEY"), os.Getenv("S3SECKEY"), os.Getenv("REGION"), os.Getenv("BUCKETNAME"))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(myS3)
+
+		path := c.DefaultQuery("path", "")
+
+		file, _, err := c.Request.FormFile("file")
+		fmt.Println(file)
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+
+		params := &s3.PutObjectInput{
+			Bucket: aws.String(myS3.BucketName),  // Required
+			Key:    aws.String("nellow/" + path), // Required
+			ACL:    aws.String("public-read"),
+			Body:   file,
+		}
+
+		resp, err := myS3.Svc.PutObject(params)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(resp)
+		fmt.Println(params)
+
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
